7c249ef36cd9262324d7cacb8d7b83117e393092
thoma <73348376+tom-devv@users.noreply.github.com>
2024-08-27 01:03:05 +0100

    start oplayer

---
+++ src/main/java/com/octanepvp/splityosis/octaneengine/OPlayer.java
@@ -0,0 +1,16 @@
+package com.octanepvp.splityosis.octaneengine;
+
+import java.util.UUID;
+
+public class OPlayer {
+
+
+    private OPlayer(){
+
+    }
+
+//    public static OPlayer getOPlayer(UUID uuid) {
+//
+//    };
+
+}
